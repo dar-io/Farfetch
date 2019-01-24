7d0599631165f74b6a5847a1d1dbf82362594b34
Dario <darioramalho@gmail.com>
2019-01-24 20:50:16 +0000

    Updates

---
+++ component---src-pages-typography-js-cb3b888dc695d673b6da.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{143:function(n,e,t){"use strict";t.r(e);var a=t(6),r=t.n(a),i=t(0),u=t.n(i),p=(t(205),function(n){function e(){return n.apply(this,arguments)||this}return r()(e,n),e.prototype.render=function(){return u.a.createElement("div",null,u.a.createElement("h1",null,"This is a Large heading"),u.a.createElement("p",null,"This is a paragraph with regular type"))},e}(u.a.Component));e.default=p},205:function(n,e,t){}}]);
+//# sourceMappingURL=component---src-pages-typography-js-cb3b888dc695d673b6da.js.map
